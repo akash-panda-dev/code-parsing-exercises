1a05d85bd5bbd2d0dc16dd6d9b7f5a1aef3e8b2f
Akash Panda <akashajaypanda@gmail.com>
2024-04-13 13:40:02 -0700

    Bunch of code parsing

--- code_parser.py
+++ code_parser.py
@@ -1,20 +1,23 @@
-from typing import Tuple, List
 from enum import Enum
+from typing import List, Tuple
 from unittest import TestCase, main
 
+
 class TokenName(Enum):
-    NAME = 'NAME'
-    ASSIGN = 'ASSIGN'
-    OP = 'OP'
-    NUM = 'NUM'
+    NAME = "NAME"
+    ASSIGN = "ASSIGN"
+    OP = "OP"
+    NUM = "NUM"
+
 
 CHAR_TO_TOKEN_NAME_MAP = {
-    '+': TokenName.OP,
-    '=': TokenName.ASSIGN,
-    '*': TokenName.OP,
-    '/': TokenName.OP,
-    '-': TokenName.OP,
+    "+": TokenName.OP,
+    "=": TokenName.ASSIGN,
+    "*": TokenName.OP,
+    "/": TokenName.OP,
+    "-": TokenName.OP,
 }
+
 
 class Tokenizer:
     def __init__(self, input_str: str):
@@ -24,14 +27,14 @@ class Tokenizer:
 
     def __iter__(self):
         return self
-    
+
     def __next__(self):
         token = self.get_next_token()
         if token is None:
             raise StopIteration
-        
+
         return (token[0].name, token[1])
-    
+
     def advance(self):
         self.pos += 1
         if self.pos < len(self.input):
@@ -42,7 +45,7 @@ class Tokenizer:
     def skip_whitespace(self):
         while self.cur_char is not None and self.cur_char.isspace():
             self.advance()
-        
+
     def parse_num(self):
         result = ""
         while self.cur_char is not None and self.cur_char.isdigit():
@@ -69,21 +72,113 @@ class Tokenizer:
                 token = self.cur_char
                 token_type = CHAR_TO_TOKEN_NAME_MAP[token]
                 self.advance()
-                return token_type, token 
+                return token_type, token
             else:
                 raise ValueError(f"Invalid character: {self.cur_char}")
 
         return None
 
+
 def tokenize(text: str):
     tokenizer = Tokenizer(text)
     return list(tokenizer)
 
+
 def test_tokenize():
     assert tokenize("spam = x + 34 * 567") == [
-        ('NAME', 'spam'), ('ASSIGN', '='), ('NAME', 'x'), 
-        ('OP', '+'), ('NUM', '34'), ('OP', '*'), ('NUM', '567')
+        ("NAME", "spam"),
+        ("ASSIGN", "="),
+        ("NAME", "x"),
+        ("OP", "+"),
+        ("NUM", "34"),
+        ("OP", "*"),
+        ("NUM", "567"),
     ]
 
-if __name__=="__main__":
+
+SPACE = " "
+
+
+def to_source(tree, node_i=0, code=None):
+    if code is None:
+        code = []
+    if node_i >= len(tree):
+        return "".join(code)
+    node = tree[node_i]
+
+    if isinstance(node, tuple):
+        code.append(to_source(node))
+    elif node == "assign":
+        code.append(to_source(tree, node_i + 1))
+        code.append(" = ")
+        code.append(to_source(tree, node_i + 2))
+    elif node == "binop":
+        code.append(to_source(tree, node_i + 2))
+        code.append(" ")
+        code.append(to_source(tree, node_i + 1))
+        code.append(" ")
+        code.append(to_source(tree, node_i + 3))
+    elif node == "name" or node == "num":
+        code.append(to_source(tree, node_i + 1))
+    else:
+        code.append(node)
+
+    return "".join(code)
+
+
+def test_to_source():
+    tree = (
+        "assign",
+        "spam",
+        ("binop", "+", ("name", "x"), ("binop", "*", ("num", "34"), ("num", "567"))),
+    )
+    assert to_source(tree) == "spam = x + 34 * 567"
+
+
+def simplify_tree(tree, node_i=0, new_tree=None):
+    if new_tree is None:
+        new_tree = []
+    if node_i >= len(tree):
+        return tuple(new_tree)  # Convert the list back to a tuple before returning
+    node = tree[node_i]
+    if isinstance(node, tuple):
+        new_tree.append(simplify_tree(node))
+    elif node == "assign":
+        new_tree.append(node)
+        new_tree.extend(simplify_tree(tree, node_i + 1))
+        new_tree.extend(simplify_tree(tree, node_i + 2))
+    elif node == "binop":
+        op = simplify_tree(tree, node_i + 1)
+        bin1_tree = simplify_tree(tree, node_i + 2)
+        bin2_tree = simplify_tree(tree, node_i + 3)
+
+        if bin1_tree[0][0] == "num" and bin2_tree[0][0] == "num":
+            if op[0] == "*":
+                new_tree.extend(("num", int(bin1_tree[0][1]) * int(bin2_tree[0][1])))
+        else:
+            new_tree.append(node)
+            new_tree.extend(op)
+            new_tree.extend(bin1_tree)
+            new_tree.extend(bin2_tree)
+    elif node == "name" or node == "num":
+        new_tree.append(node)
+        new_tree.extend(simplify_tree(tree, node_i + 1))
+    else:
+        new_tree.append(node)
+    return tuple(new_tree)  # Ensure the final output is a tuple if needed
+
+
+def test_simplify_tree():
+    tree = ('assign', 'spam', 
+        ('binop', '+', 
+                  ('name', 'x'),
+                  ('binop', '*', ('num', 34), ('num', 567))))
+    simple_tree = simplify_tree(tree)
+    print(simple_tree)
+    assert simple_tree == \
+    ('assign', 'spam', ('binop', '+', ('name', 'x'), ('num', 19278)))
+
+if __name__ == "__main__":
     test_tokenize()
+    test_to_source()
+    test_simplify_tree()
